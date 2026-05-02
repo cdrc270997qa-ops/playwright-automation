489cd3f6109d9b2aa6f265dc4117e7ebaeb12ad9
cdrc270997qa-ops <cdrc270997qa@gmail.com>
2026-05-02 11:50:18 +0200

    Github workflow with allure report

--- tests/setup/auth.setup.ts
+++ tests/setup/auth.setup.ts
@@ -3,8 +3,8 @@ import { expect, test as setup } from '@playwright/test';
 setup('authenticate user', async ({ page }) => {
   await page.goto('/login');
 
-  await page.getByLabel('Username').fill(process.env.USERNAME!);
-  await page.getByLabel('Password').fill(process.env.PASSWORD!);
+  await page.getByLabel('Username').fill('practice');
+  await page.getByLabel('Password').fill('SuperSecretPassword!');
   await page.getByRole('button', { name: 'Login' }).click();
 
   await expect(page.locator('text=You logged into a secure area!')).toBeVisible();
